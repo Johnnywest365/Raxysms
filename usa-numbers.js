ddd7257c8934e501f454f7b4b81c993e7bf28170
Johnnywest365 <omoboyejohnoke@gmail.com>
2026-07-13 15:54:25 +0100

    Update Firebase SDK to version 12.4.0

--- usa-numbers.js
+++ usa-numbers.js
@@ -7,14 +7,14 @@ import { auth, db } from "./firebase.js";
 
 import {
     onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+} from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
 
 import {
     collection,
     getDocs,
     doc,
     getDoc
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+} from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
 
 /* ==========================================
    DEBUG
